5af3a3633ebc891ff86f6becbeb82c73a5428215
eshaan <eshdesh@null.net>
2024-12-12 13:50:14 -0600

    Update readLine.java

--- readLine.java
+++ readLine.java
@@ -42,16 +42,4 @@ public class readLine {
         }
         return scanner.nextBoolean();
     }
-
-    public static void main(String[] args) {
-        String name = readLine("What is your name? ");
-        int age = readInt("What is your age? ");
-        double height = readDouble("What is your height in meters? ");
-        boolean finishedWork = readBoolean("Is your work done? ");
-
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Height: " + height);
-        System.out.println("Finished work: " + finishedWork);
-    }
 }
